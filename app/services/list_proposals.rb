218585da4fc46e8875fac5376f387f63e6f77372
Anderson Limas <andersonlimacode@gmail.com>
2019-06-16 05:07:34 +0000

    Add list proposals service
    - add implementation to extract the list of proposals from a file_path
    - add tests for all necessary contexts
    - add sample of proposals.txt file
    - add sample of proposals_empty.txt file
    
    Signed-off-by: Anderson Limas <andersonlimacode@gmail.com>

---
+++ app/services/list_proposals.rb
@@ -0,0 +1,27 @@
+module Services
+  class ListProposals
+
+    def initialize(proposal_path)
+      @proposal_path = proposal_path
+    end
+
+    def call
+      validate_proposal_path
+
+      File.readlines(proposal_path).map(&:strip)
+    end
+
+    private
+
+    attr_reader :proposal_path
+
+    def validate_proposal_path
+      raise_msg('Invalid Proposal Path')        unless File.exist?(proposal_path)
+      raise_msg('Invalid Proposal: Empty file') if     File.empty?(proposal_path)
+    end
+
+    def raise_msg(msg)
+      raise ArgumentError.new(msg)
+    end
+  end
+end
---
+++ test/app/services/list_proposals_test.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require './app/services/list_proposals'
+
+describe ::Services::ListProposals do
+
+  describe 'when given an invalid proposal_path' do
+
+    it 'raise an error' do
+      subject = ::Services::ListProposals.new('invalid_path')
+
+      error = lambda { subject.call }.must_raise ArgumentError
+
+      _(error.message).must_equal 'Invalid Proposal Path'
+    end
+  end
+
+  describe 'when given a valid proposal_path' do
+    describe 'when proposal file is empty' do
+
+      it 'raise an error' do
+        subject = ::Services::ListProposals.new('test/support/proposals_empty.txt')
+
+        error = lambda { subject.call }.must_raise ArgumentError
+
+        _(error.message).must_equal 'Invalid Proposal: Empty file'
+      end
+    end
+
+    describe 'when proposal file has proposals' do
+
+      it 'return a list of proposals' do
+        subject = ::Services::ListProposals.new('test/support/proposals.txt')
+
+        expected_result = [
+          'How to be Roman 60min',
+          'How to be rich 45min',
+          'Not anymore 30min',
+          'Ruby is Awesome lightning',
+          'Erros comuns em Ruby 45min'
+        ]
+
+        _(subject.call).must_equal expected_result
+      end
+    end
+  end
+end
