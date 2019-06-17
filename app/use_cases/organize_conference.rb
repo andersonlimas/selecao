d86f8d92dfc9224fca6a7bcfb545630f26d0413f
Anderson Limas <andersonlimacode@gmail.com>
2019-06-17 21:22:17 +0000

    Refactor OrganizeConference UseCase
    - update usecase to generate the tracks dynamic
    - update tests for the use case
    
    Signed-off-by: Anderson Limas <andersonlimacode@gmail.com>

--- app/use_cases/organize_conference.rb
+++ app/use_cases/organize_conference.rb
@@ -15,23 +15,31 @@ module UseCases
       proposal_list          = provide_list_proposals(proposals_path).call
       lecture_list           = provide_create_lectures(proposal_list).call
 
-      morning_session        = provide_create_morning_session(lecture_list).call
-      remaining_lecture_list = lecture_list - morning_session.lectures
-      afternoon_session      = provide_create_afternoon_session(remaining_lecture_list).call
-      track_a                = provide_create_track('Track A', morning_session, afternoon_session).call
+      tracks = create_tracks(lecture_list)
 
-      remaining_lecture_list = remaining_lecture_list  - afternoon_session.lectures
-      morning_session        = provide_create_morning_session(remaining_lecture_list).call
-      remaining_lecture_list = remaining_lecture_list - morning_session.lectures
-      afternoon_session      = provide_create_afternoon_session(remaining_lecture_list).call
-      track_b                = provide_create_track('Track B', morning_session, afternoon_session).call
-
-      provide_create_conference(track_a, track_b)
+      provide_create_conference(tracks)
     end
 
     private
 
     attr_reader :proposals_path
+
+    def create_tracks(lecture_list, index = 0, tracks = [])
+      track_names = ('A'..'Z').to_a
+
+      morning_session        = provide_create_morning_session(lecture_list).call
+      remaining_lecture_list = lecture_list - morning_session.lectures
+      afternoon_session      = provide_create_afternoon_session(remaining_lecture_list).call
+      remaining_lecture_list = remaining_lecture_list  - afternoon_session.lectures
+
+      tracks << provide_create_track("Track #{track_names[index]}", morning_session, afternoon_session).call
+
+      return tracks if remaining_lecture_list.empty?
+
+      index += 1
+
+      create_tracks(remaining_lecture_list, index, tracks)
+    end
 
     def provide_list_proposals(proposals_path)
       ::Services::ListProposals.new(proposals_path)
@@ -53,7 +61,7 @@ module UseCases
       ::Services::CreateTrack.new(track_name, morning_session, afternoon_session)
     end
 
-    def provide_create_conference(*tracks)
+    def provide_create_conference(tracks)
       tracks.map(&:to_s).join("\n")
     end
   end
--- test/app/use_cases/organize_conference_test.rb
+++ test/app/use_cases/organize_conference_test.rb
@@ -20,45 +20,49 @@ describe ::UseCases::OrganizeConference, '#call' do
 
 10:45 Apresentando Lua para as massas 30min
 
+11:15 Erros de Ruby oriundos de versões erradas de gems 45min
+
 12:00 Almoço
 
-13:00 Erros de Ruby oriundos de versões erradas de gems 45min
+13:00 Erros comuns em Ruby 45min
 
-13:45 Erros comuns em Ruby 45min
+13:45 Rails para usuários de Django lightning
 
-14:30 Rails para usuários de Django lightning
+13:50 Trabalho remoto: prós e cons 60min
 
-14:35 Trabalho remoto: prós e cons 60min
+14:50 Desenvolvimento orientado a gambiarras 45min
 
-15:35 Desenvolvimento orientado a gambiarras 45min
+15:35 Aplicações isomórficas: o futuro (que talvez nunca chegaremos) 30min
 
-16:20 Aplicações isomórficas: o futuro (que talvez nunca chegaremos) 30min
+16:05 Codifique menos, Escreva mais! 30min
 
-16:50 Evento de Networking
+16:35 Evento de Networking
 
 #### Track B:
-9:00 Codifique menos, Escreva mais! 30min
+9:00 Programação em par 45min
 
-9:30 Programação em par 45min
+9:45 A mágica do Rails: como ser mais produtivo 60min
 
-10:15 A mágica do Rails: como ser mais produtivo 60min
+10:45 Ruby on Rails: Por que devemos deixá-lo para trás 60min
 
 12:00 Almoço
 
-13:00 Ruby on Rails: Por que devemos deixá-lo para trás 60min
+13:00 Clojure engoliu Scala: migrando minha aplicação 45min
 
-14:00 Clojure engoliu Scala: migrando minha aplicação 45min
+13:45 Ensinando programação nas grotas de Maceió 30min
 
-14:45 Ensinando programação nas grotas de Maceió 30min
+14:15 Ruby vs. Clojure para desenvolvimento backend 30min
 
-15:15 Ruby vs. Clojure para desenvolvimento backend 30min
+14:45 Manutenção de aplicações legadas em Ruby on Rails 60min
 
-15:45 Manutenção de aplicações legadas em Ruby on Rails 60min
+15:45 Um mundo sem StackOverflow 30min
+
+16:15 Otimizando CSS em aplicações Rails 30min
 
 16:45 Evento de Networking
       DESC
 
-      _(conference.to_s).must_equal expected_track_desc
+      _(conference).must_equal expected_track_desc
     end
   end
 
